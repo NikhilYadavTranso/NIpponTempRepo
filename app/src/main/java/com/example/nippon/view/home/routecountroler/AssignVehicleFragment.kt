71a3382abc77a5622f20f6259916552965c4cafb
Sidharth Sahoo <sidharthsahoodev@gmail.com>
2022-03-01 12:19:34 +0530

    Route Controller Serializable and Assign Vehicle Fragment Logic Done

--- app/src/main/java/com/example/nippon/view/home/routecountroler/AssignVehicleFragment.kt
+++ app/src/main/java/com/example/nippon/view/home/routecountroler/AssignVehicleFragment.kt
@@ -6,13 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.navArgs
 import com.example.nippon.R
 import com.example.nippon.databinding.AssignVehicleFragmentBinding
 import com.example.nippon.databinding.RouteControllerFragmentBinding
+import com.example.nippon.model.SerialData
 
 class AssignVehicleFragment : Fragment() {
     private lateinit var assignVehicleFragmentBinding: AssignVehicleFragmentBinding
-
+    private lateinit var serialData: SerialData
 
     private lateinit var viewModel: AssignVehicleViewModel
 
@@ -22,7 +25,12 @@ class AssignVehicleFragment : Fragment() {
     ): View? {
         assignVehicleFragmentBinding = AssignVehicleFragmentBinding.inflate(inflater)
 
+        serialData = arguments?.getSerializable("myArg") as SerialData
+        assignVehicleFragmentBinding.assignVehicleRequestId.setText(serialData.request.Requestid)
 
+        assignVehicleFragmentBinding.submitAssignVehicle.setOnClickListener {
+            Toast.makeText(assignVehicleFragmentBinding.root.context, "Vehicle Assigned", Toast.LENGTH_SHORT).show()
+        }
         return assignVehicleFragmentBinding.root
     }
 
--- app/src/main/java/com/example/nippon/view/home/routecountroler/RouteControllerFragment.kt
+++ app/src/main/java/com/example/nippon/view/home/routecountroler/RouteControllerFragment.kt
@@ -1,24 +1,18 @@
 package com.example.nippon.view.home.routecountroler
 
-import android.app.DownloadManager
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.nippon.R
-import com.example.nippon.databinding.HomeFragmentBinding
 import com.example.nippon.databinding.RouteControllerFragmentBinding
 import com.example.nippon.model.SerialData
 import com.example.nippon.model.request.Requests
-import com.example.nippon.view.home.HomeDirections
-import com.example.nippon.view.login.LoginFragmentDirections
 
 class RouteControllerFragment : Fragment() {
 
@@ -36,10 +30,7 @@ class RouteControllerFragment : Fragment() {
             override fun onCustomItemClicked(request: Requests) {
 
                 val passingRequests = SerialData(request)
-                Toast.makeText(activity, "Feature under development", Toast.LENGTH_SHORT).show()
-                val action = RouteControllerFragmentDirections.actionRouteControllerFragmentToAssignVehicleFragment()
-                val bundle = bundleOf("passingRequests" to passingRequests)
-
+                val action = RouteControllerFragmentDirections.actionRouteControllerFragmentToAssignVehicleFragment(passingRequests)
                 Navigation.findNavController(routeControllerFragmentbinding.routeRecycler).navigate(action)
 
             }
